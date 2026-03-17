77cf7ec4fdb5393fa3777bbd6e1bdf069c4567af
s32877 <s32877@pjwstk.edu.pl>
2026-03-17 14:49:53 +0100

    Added Equipment abstract class and EquipmentStatus enum

---
+++ Models/Equipment.cs
@@ -0,0 +1,19 @@
+﻿namespace APBD_TASK2.Models;
+
+public abstract class Equipment
+{
+    private static int nextId = 1;
+    public int Id { get;  }
+    public string Name { get;  }
+    public string Description { get;  }
+    
+    public DateTime AddedDate { get; set; }
+
+    public Equipment(string name, string description = "")
+    {
+        Id = nextId++;
+        Name = name;
+        Description = description;
+        AddedDate = DateTime.Now;
+    }
+}
