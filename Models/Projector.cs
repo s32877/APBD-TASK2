5dcf4c7d5cce1ab39b7731726c03251034fd60aa
s32877 <s32877@pjwstk.edu.pl>
2026-03-17 15:20:39 +0100

    Added projector

---
+++ Models/Projector.cs
@@ -0,0 +1,18 @@
+﻿namespace APBD_TASK2.Models;
+
+public class Projector : Equipment
+{
+    public int Resulution {get; set;}
+    public int Width {get; set;}
+    public int Height {get; set;}
+    public int Length {get; set;}
+
+    public Projector(string name, int resulution, int width, int height, int length)
+        : base(name)
+    {
+        Resulution = resulution;
+        Width = width;
+        Height = height;
+        Length = length;
+    }
+}
