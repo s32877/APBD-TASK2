49f87365897abddfb09d536173202af5802cb066
s32877 <s32877@pjwstk.edu.pl>
2026-03-17 14:53:30 +0100

    Added Laptop

---
+++ Models/Laptop.cs
@@ -0,0 +1,14 @@
+﻿namespace APBD_TASK2.Models;
+
+public class Laptop : Equipment
+{
+    public int RamGb { get; set; }
+    public int ScreenSize { get; set; }
+
+    public Laptop(string name, int ram, int screen)
+        : base(name)
+    {
+        RamGb = ram;
+        ScreenSize = screen;
+    }
+}
