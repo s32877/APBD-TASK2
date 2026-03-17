357ab2467adcd97e525a249d729ec9b2f2f59d68
s32877 <s32877@pjwstk.edu.pl>
2026-03-17 15:22:36 +0100

    Added camera

---
+++ Models/Camera.cs
@@ -0,0 +1,14 @@
+﻿namespace APBD_TASK2.Models;
+
+public class Camera : Equipment
+{
+    public int MegaPixels { get; set; }
+    public int NumberOfModes { get; set; }
+    public int MaxResolution { get; set; }
+    public Camera(string name, int megaPixels, int numberOfModes, int maxResolution) : base(name)
+        {
+        MegaPixels = megaPixels;
+        NumberOfModes = numberOfModes;
+        MaxResolution = maxResolution;
+        }
+}
