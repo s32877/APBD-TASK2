8a76122851feb8c25bd700492ebc3bb9b4855eef
s32877 <s32877@pjwstk.edu.pl>
2026-03-17 15:00:40 +0100

    Added user

---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+﻿using APBD_TASK2.Enum;
+
+namespace APBD_TASK2.Models;
+
+public class User
+{
+    public static int nextId = 1;
+    
+    public int Id { get; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public UserType Type { get; set; }
+
+    public int MaxActiveRentails => Type switch
+    {
+        UserType.Student => 2,
+        UserType.Employee => 5,
+        _ => 0
+    };
+
+    public User(string name, string surname, UserType type)
+    {
+        Id = nextId++;
+        Name = name;
+        Surname = surname;
+        Type = type;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,5 +5,5 @@ using APBD_TASK2.Models;
 
 var db = Singleton.Instance;
 
-var laptop = new Laptop("Asus F16", 2 ,13);
+var laptop = new Laptop("Asus TUF F16", 2 ,13);
 db.EquipmentList.Add(laptop);
